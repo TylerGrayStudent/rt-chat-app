1d7bfb4345aeefd40dc110eb86950f4e567d416b
Tyler Gray <grayt@g.cofc.edu>
2022-04-23 09:27:43 -0400

    fix build

---
+++ components/MessageInput/MessageInput.tsx
@@ -0,0 +1,14 @@
+interface Props {
+  //
+}
+
+const MessageInput: React.FC<Props> = () => {
+  return (
+    <div>
+      <input type="text" />
+      <button>Send</button>
+    </div>
+  );
+};
+
+export default MessageInput;

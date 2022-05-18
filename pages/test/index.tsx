17917ad4ec38e4d94c69b196e2a0cdf8dc928e29
Tyler Gray <wymngray@gmail.com>
2022-05-18 19:28:16 -0400

    wip

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,29 +1,34 @@
 import { NextPage } from "next";
 import { Paper } from "@mui/material";
-import { useRouter } from "next/router";
-import { useContext, useEffect, useRef, useState } from "react";
-import { motion } from "framer-motion";
-import SocketProvider, {
-  SocketContext,
-  useSocket,
-} from "../../context/sockets";
-import { auth } from "../../firebase/firebase";
+import { useEffect, useState } from "react";
+import { io, Socket } from "socket.io-client";
 
 const TestPage: NextPage = () => {
-  const videoRef = useRef<HTMLVideoElement>(null);
+  //const videoRef = useRef<HTMLVideoElement>(null);
   //const socket = useSocket();
 
-  const socket = useContext(SocketContext);
+  //const socket = useContext(SocketContext);
 
-  const a = auth;
+  const [socket, setSocket] = useState<Socket>();
+
   const [whoCall, setWhoCall] = useState("");
-  socket?.on("call", (data: any) => {
-    console.log(data);
-  });
 
   useEffect(() => {
-    getVideo();
-  }, [videoRef, auth]);
+    const newSocket = io("http://localhost:3001");
+    setSocket(newSocket);
+
+    newSocket.on("connect", () => {
+      console.log("connected");
+    });
+
+    newSocket.on("call", (data: any) => {
+      console.log(data);
+    });
+
+    return () => {
+      newSocket.close();
+    };
+  }, []);
 
   const makeCall = () => {
     console.log(socket);
@@ -46,29 +51,30 @@ const TestPage: NextPage = () => {
   };
 
   const toggleVideo = () => {
-    let video = videoRef.current;
-    if (!video) return;
-    if (!video.paused) {
-      video.pause();
-    } else {
-      video.play();
-    }
+    // let video = videoRef.current;
+    // if (!video) return;
+    // if (!video.paused) {
+    //   video.pause();
+    // } else {
+    //   video.play();
+    // }
   };
 
   return (
-    <SocketProvider username={"t"}>
-      <Paper className="h-full grid justify-center content-center">
-        <motion.video drag ref={videoRef}></motion.video>
-        <button onClick={() => toggleVideo()}>Toggle</button>
-        <button onClick={() => makeCall()}>Make call</button>
-        <input
-          type="text"
-          value={whoCall}
-          onChange={(e) => setWhoCall(e.target.value)}
-        />
-      </Paper>
-    </SocketProvider>
+    // <SocketProvider username={"t"}>
+    <Paper className="h-full grid justify-center content-center">
+      {/* <motion.video drag ref={videoRef}></motion.video> */}
+      <button onClick={() => toggleVideo()}>Toggle</button>
+      <button onClick={() => makeCall()}>Make call</button>
+      <input
+        type="text"
+        value={whoCall}
+        onChange={(e) => setWhoCall(e.target.value)}
+      />
+    </Paper>
+    // </SocketProvider>
   );
 };
 
 export default TestPage;
+
